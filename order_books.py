ce97bd685d47f5a301d37951d290d293d1ab10fc
Michael Widner <mikewidner@stanford.edu>
2017-11-03 13:54:12 -0700

    Cross refs and copies returned

--- order_books.py
+++ order_books.py
@@ -5,6 +5,7 @@ Mike Widner <mikewidner@stanford.edu>
 '''
 
 import os
+import re
 import csv
 import sys
 import string
@@ -46,6 +47,7 @@ def fill_empties(df):
 	for col in columns:
 		df[col].fillna("", inplace=True)
 
+
 def convert_dates(df):
 	''' Convert columns with date into date types '''
 	columns = ['Date Order Received', 'Date Order Fulfilled', 'Date Payment Received']
@@ -58,6 +60,10 @@ def find_crossrefs(df):
 	''' 
 	Traverse the cross-references given to figure out empty dates 
 	'''
+	# Find numbers in Purchaser field
+	pattern = r"see (\d+)"
+	df['Cross Reference'] = df['Purchaser'].str.extract(pattern, flags=re.IGNORECASE, expand=True)
+	return df
 
 
 def subtract_returned_copies(df):
@@ -66,13 +72,12 @@ def subtract_returned_copies(df):
 	make the copies ordered a negative number
 	sums should work correctly then
 	'''
-	pass
-
-
-# def write_work_puchaser_edges(df, outpath):
-# 	# TODO: Add header source, target, weight
-# 	df[['Title', 'Purchaser', 'Copies Ordered']].groupby(['Title', 'Purchaser']).sum().to_csv(outpath)
-
+	pattern1 = r"retd\.? (\d+)"
+	pattern2 = r"(\d+) (?:retd|\"|rd)"
+	# Using two patterns to avoid having possible multiple groups
+	df['Copies Returned'] = df['Purchaser'].str.extract(pattern1, flags=re.IGNORECASE, expand=False)
+	df['Copies Returned'] = df['Purchaser'].str.extract(pattern2, flags=re.IGNORECASE, expand=False)
+	return df
 
 def sum_pounds_shillings_pence(df):
 	pass
@@ -82,7 +87,8 @@ def clean_purchaser_entries(df):
 	'''
 	Regularize entries for purchasers
 	'''
-	df = df.sample(n=500)
+	# ADD: Tokenize names, if first token matches, do edit distance of second token
+	# if close, automatically join; use longer label?
 	pattern = r",.+$"
 	df['Purchaser'] = df['Purchaser'].str.replace(pattern, '')
 	pattern = r"\([^)]+\)$|\[[^\]]+\]" # remove ()[] at end of entry
@@ -99,7 +105,7 @@ def clean_purchaser_entries(df):
 
 def write_purchasers(df, outpath):
 	df['Purchaser'] = df['Purchaser'].astype(str)
-	df = clean_purchaser_entries(df)
+	# df = clean_purchaser_entries(df)
 	with open(os.path.join(outpath, 'purchasers.txt'), 'w') as outfile:
 		for purchaser in sorted(df['Purchaser'].unique()):
 			outfile.write(str(purchaser) + "\n")
@@ -113,13 +119,14 @@ def write_hand_date_ranges(df, outpath):
 
 
 def load_dataframe(inputfile):
-	df = pd.read_csv(options.input, quoting=0, 
+	df = pd.read_csv(inputfile, quoting=0, 
 		parse_dates=['Date Order Received', 'Date Order Fulfilled', 'Date Payment Received'], 
 		dayfirst=True)
 	fill_empties(df)
 	df = convert_dates(df)
 	df['Copies Ordered'].apply(pd.to_numeric, errors='raise')
 	return df
+
 
 def write_work_purchaser_edges(df, filename):
 	# TODO: Add header source, target, weight
@@ -129,19 +136,29 @@ def write_work_purchaser_edges(df, filename):
 		filename = 'work_purchaser_edges_' + str(int(year)) + '.csv'
 		group.groupby(['Title', 'Purchaser']).sum().to_csv(filename)
 
+
+def write_processed_data(df, infile, outpath):
+	basename = os.path.basename(infile)
+	(root, ext) = os.path.splitext(basename)
+	df.to_csv(os.path.join(outpath, root + '_processed' + ext))
+
+
 def main():
 	options = parse_options()
 	check_output_path(options.output)
 	df = load_dataframe(options.input)
+	df = find_crossrefs(df)
+	df = subtract_returned_copies(df)
+	# print(df[~df['Cross Reference'].isnull()].sample(n=100))
+	write_processed_data(df, options.input, options.output)
 
+	quit()
 	write_purchasers(df, options.output)
-	write_work_purchaser_edges(df, os.path.join(options.output, filename))
-
-
+	# write_work_purchaser_edges(df, os.path.join(options.output, filename))
 
 
 if __name__ == '__main__':
     if sys.version_info < (3, 0):
-        print("This script requires Python 3")
+        print("This script requires Python 3.")
         exit(-1)
     main()
