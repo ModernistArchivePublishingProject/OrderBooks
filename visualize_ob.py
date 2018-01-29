ea4b4b774685566b03017f6c67cf305084f57ac2
cheriexu <cxuhan@stanford.edu>
2018-01-28 20:21:18 -0800

    worked on income aggregate

--- visualize_ob.py
+++ visualize_ob.py
@@ -11,6 +11,7 @@ import string
 import pandas as pd
 import matplotlib.pyplot as plt
 import collections
+import datetime
 
 
 #import csv, read in data frames, create arrays 
@@ -25,19 +26,28 @@ count = dict.fromkeys(titles,0)
 # Get sales count for each book titles
 # Get two dfs
 copies = {}
+
 #df['Copies Ordered'].convert_objects(convert_numeric=True)
 #print(df['Copies Ordered'].sum())
-df['Date Order Received'] = pd.to_datetime(df['Date Order Received'], errors = 'coerce', origin = '1920-01-01')
+#df['Date Order Received'] = pd.to_datetime(df['Date Order Received'], errors = 'coerce',origin = pd.Timestamp('1920-01-01'))
 df['Copies Ordered'] = pd.to_numeric(df['Copies Ordered'], errors='coerce')
 df['Pence'] = pd.to_numeric(df['Pence'], errors='coerce')
 df['Pounds'] = pd.to_numeric(df['Pounds'], errors='coerce')
 df['Shillings'] = pd.to_numeric(df['Shillings'], errors='coerce')
-df[['Pence','Pounds','Shillings']].fillna(0)
-print(df [['Pence','Pounds','Shillings']])
+df.fillna(0)
+nonull = df[df['Date Order Received'].notnull()]
+#money = nonull[['Pence','Pounds','Shillings']]
+#dates = nonull['Date Order Received']
+
 #df.groupby(['Title'])[['Copies Ordered']].sum().plot.bar(title = 'Number of Transactions per book')
 #print (df.groupby(['Title', pd.Grouper(key='Date Order Received', freq='M')])[['Copies Ordered']].sum())
 #counts.plot(kind='bar',title = 'Bar chart for book sales')
 #plt.show()
 #Show income per month
-print (df.groupby([pd.Grouper(key='Date Order Received', freq='M')])[['Pence','Pounds','Shillings']].sum())
+nonull = nonull[nonull['Date Order Received'] < datetime.date.today()  ]
+nonull = nonull[nonull['Date Order Received']  > datetime.datetime(1910,1,1)]
+income = nonull.groupby([pd.Grouper(key='Date Order Received', freq='M')])[['Pence','Pounds','Shillings']].sum()
+print(income)
+#TODO, convert Pound, pence, Shillings into one unified unit
+
 
