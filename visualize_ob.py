e037887ffa5f7dcc86e4f6ee91a08b2de1c832a1
cheriexu <cxuhan@stanford.edu>
2018-01-29 15:17:25 -0800

    Added cumulative line plot for income

--- visualize_ob.py
+++ visualize_ob.py
@@ -27,6 +27,7 @@ titles = df['Title'].unique()
 #Total Copies sold of each book
 def plotTotalSale(df):
 	df['Copies Ordered'] = pd.to_numeric(df['Copies Ordered'], errors='coerce')
+	plt.figure(0)
 	df.groupby(['Title'])[['Copies Ordered']].sum().plot.bar(title = 'Number of Copies Sold')
 	plt.xlabel('Book Titles', fontsize = 10)
 	plt.ylabel('Copies Sold', fontsize = 10)
@@ -42,19 +43,26 @@ def computeIncome(df):
 	nonull = nonull[nonull['Date Order Received'] < datetime.date.today()  ]
 	nonull = nonull[nonull['Date Order Received']  > datetime.datetime(1910,1,1)]
 	income = nonull.groupby([pd.Grouper(key='Date Order Received', freq='M')])[['Pence','Pounds','Shillings']].sum()
-	label =income.index.get_level_values('Date Order Received')
+	labels =income.index.get_level_values('Date Order Received')
 	#Convert Pound, pence, Shillings into one unified unit
 	# 12 pence in a shilling and 20 shillings, or 240 pence, in a pound
 	income['total'] = income['Pounds'] + income['Pence']/240.0 + income['Shillings']/20.0
+	plt.figure(1)
 	income['total'].plot.bar(title = 'Monthly Income')
-	index = np.arange(0,len(label),6)
-	label = label[0:len(label):6]
+	index = np.arange(0,len(labels),6)
+	label = labels[0:len(labels):6]
 	plt.xticks( index,label, fontsize=5, rotation=30)
 	plt.xlabel('Date', fontsize=10)
-	plt.ylabel('Pound', fontsize =10 )
+	plt.ylabel('Income (Pound)',  fontsize =10)
+	#Add Cum sum income
+	plt.figure(2)
+	income['total'].cumsum().plot(title='Cumulative Income: 1927-1946')
+	#plt.xticks( index,labels, fontsize=5, rotation=30)
+	plt.xlabel('Date', fontsize=10)
+	plt.ylabel('Income (Pound)',  fontsize =10)
 	plt.show()
 
 
-plotTotalSale(df)
+#plotTotalSale(df)
 computeIncome(df)
 
