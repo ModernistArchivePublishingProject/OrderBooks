56ddfb1860c5218dee8a72f5f9d8e27434608bcb
cheriexu <cxuhan@stanford.edu>
2018-02-05 15:37:19 -0800

    Finish stacked bar chart and difference between order dates Found discrepancies in data Need to figure out a fix

--- visualize_ob.py
+++ visualize_ob.py
@@ -19,13 +19,18 @@ inputfile = './output.csv/obeverything_processed.csv'
 df = pd.read_csv(inputfile, quoting=0, 
 		parse_dates=['Date Order Received', 'Date Order Fulfilled', 'Date Payment Received'], 
 		dayfirst=True)
-#get unique book names
-titles = df['Title'].unique()
-#print(titles)
 df['Copies Ordered'] = pd.to_numeric(df['Copies Ordered'], errors='coerce')
 df['Pence'] = pd.to_numeric(df['Pence'], errors='coerce')
 df['Pounds'] = pd.to_numeric(df['Pounds'], errors='coerce')
 df['Shillings'] = pd.to_numeric(df['Shillings'], errors='coerce')
+
+
+#TODO Write function to collapse titles to clean up bar charts
+def collapseTitles(df):
+	#get unique book names
+	titles = df['Title'].unique()
+	#print(titles)
+	pass
 
 #Total Copies sold of each book
 def plotTotalSale(df):
@@ -60,6 +65,19 @@ def computeIncome(df):
 	plt.xlabel('Date', fontsize=10)
 	plt.ylabel('Income (Pound)',  fontsize =10)
 	#plt.savefig('cumsumIncome.png')
+	
+	#stacked bar charts for total income per month and per year showing which work contributes the most
+	fig = plt.figure(3)
+	incomeByTitle = nonull.groupby([pd.Grouper(key='Date Order Received', freq='Y'), 'Title'])[['Pence','Pounds','Shillings']].sum()
+	incomeByTitle['total'] = incomeByTitle['Pounds'] + incomeByTitle['Pence']/240.0 + incomeByTitle['Shillings']/20.0
+	unstacked = incomeByTitle['total'].unstack('Title')
+	unstacked.plot.bar(title = 'Yearly Income stacked bar char', stacked = True)
+	plt.xticks( fontsize=5, rotation=30)
+	incomeByTitleM = nonull.groupby([pd.Grouper(key='Date Order Received', freq='M'), 'Title'])[['Pence','Pounds','Shillings']].sum()
+	incomeByTitleM['total'] = incomeByTitleM['Pounds'] + incomeByTitleM['Pence']/240.0 + incomeByTitleM['Shillings']/20.0
+	unstacked = incomeByTitleM['total'].unstack('Title')
+	unstacked.plot.bar(title = 'Monthly Income stacked bar char', stacked = True)
+	plt.xticks( index,label, fontsize=5, rotation=30)
 	plt.show()
 
 #barchart for purchaser by volume
@@ -70,12 +88,24 @@ def purchaserVolume(df):
 	volumebP = volume[volume >THRESHOLD]
 	volumebP =volumebP.dropna()
 	# plot chart
-	fig = plt.figure(3)
+	fig = plt.figure(4)
 	volumebP.plot.pie(y = 'Copies Ordered',labels = volumebP.index.get_level_values('Purchaser'), legend = False,  autopct='%1.1f%%')
 	#plt.savefig('volume.png')
 	plt.show()
 
-plotTotalSale(df)
-computeIncome(df)
-purchaserVolume(df)
+#Chart showing average, min, and max time between order filled and payment received per work and total
+def diffFilledPayment(df):
+	df['Date Order Fulfilled'] = pd.to_datetime(df['Date Order Fulfilled'])
+	df['Date Payment Received'] = pd.to_datetime(df['Date Payment Received'])
+	df['diff'] = df['Date Payment Received'] - df['Date Order Fulfilled']
+	print(df['diff'].describe())
+	print(df[df['diff'].max()== df['diff']])
+	#Found weird discrepancies in data: Date Payment received parsed incorrectly with dates not matching the spreadsheet
+	weirdEntries = df[df['Date Payment Received'] > datetime.datetime(1950,1,1)]
+
+
+#plotTotalSale(df)
+#computeIncome(df)
+#purchaserVolume(df)
+diffFilledPayment(df)
 
