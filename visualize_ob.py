2b893baf3075ea6281c9c9cc36c4657ccb6270a4
cheriexu <cxuhan@stanford.edu>
2018-02-12 16:40:51 -0800

    tried to edit graphs to clean up bar graph Unsuccessfully superimpose bar and line graph due to axis misalignment create reports of book price based on marking

--- visualize_ob.py
+++ visualize_ob.py
@@ -10,6 +10,7 @@ import sys
 import string
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import collections
 import datetime
 import numpy as np
@@ -47,32 +48,35 @@ def computeIncome(df):
 	nonull = nonull[nonull['Date Order Received'] < datetime.date.today()  ]
 	nonull = nonull[nonull['Date Order Received']  > datetime.datetime(1910,1,1)]
 	income = nonull.groupby([pd.Grouper(key='Date Order Received', freq='M')])[['Pence','Pounds','Shillings']].sum()
-	labels =income.index.get_level_values('Date Order Received')
+	labels =income.index.get_level_values('Date Order Received').strftime('%m/%d/%Y')
 	incomeY = nonull.groupby([pd.Grouper(key='Date Order Received', freq='Y')])[['Pence','Pounds','Shillings']].sum()
 	#Convert Pound, pence, Shillings into one unified unit
 	# 12 pence in a shilling and 20 shillings, or 240 pence, in a pound
 	income['total'] = income['Pounds'] + income['Pence']/240.0 + income['Shillings']/20.0
-	incomeY['total'] = incomeY['Pounds'] + incomeY['Pence']/240.0 + incomeY['Shillings']/20.0
-	plt.figure(1)
-	ax = income['total'].plot.bar(title = 'Income')
-	incomeY['total'].plot.bar(ax = ax)
-	
+	#incomeY['total'] = incomeY['Pounds'] + incomeY['Pence']/240.0 + incomeY['Shillings']/20.0
+	fig = plt.figure()
+	fig.add_subplot(111)
+	ax = income['total'].plot.bar(title = 'Monthly Income',color = 'red')
+	income['total'].cumsum().plot(ax = ax, xticks = labels)
 	index = np.arange(0,len(labels),6)
 	label = labels[0:len(labels):6]
 	plt.xticks( index,label, fontsize=5, rotation=30)
+	myFmt = mdates.DateFormatter('%d')
+	#ax.xaxis.set_major_formatter(myFmt)
 	plt.xlabel('Date', fontsize=10)
-	plt.ylabel('Income (Pound)',  fontsize =10)
+	plt.ylabel('Income (Pound)',  fontsize = 10)
+	plt.show()
 	#plt.savefig('monthlyincome.png', bbox_inches='tight',dpi=100)
 	
 	#Add Cum sum income
-	fig = plt.figure(2)
+	fig = plt.figure()
 	income['total'].cumsum().plot(title='Cumulative Income: 1927-1946')
 	plt.xlabel('Date', fontsize=10)
 	plt.ylabel('Income (Pound)',  fontsize =10)
 	#plt.savefig('cumsumIncome.png', bbox_inches='tight',dpi=100)
 	
 	#stacked bar charts for total income per month and per year showing which work contributes the most
-	fig = plt.figure(3)
+	fig = plt.figure()
 	incomeByTitle = nonull.groupby([pd.Grouper(key='Date Order Received', freq='Y'), 'Title'])[['Pence','Pounds','Shillings']].sum()
 	incomeByTitle['total'] = incomeByTitle['Pounds'] + incomeByTitle['Pence']/240.0 + incomeByTitle['Shillings']/20.0
 	unstacked = incomeByTitle['total'].unstack('Title')
@@ -92,7 +96,7 @@ def purchaserVolume(df):
 	volumebP = volume[volume >THRESHOLD]
 	volumebP =volumebP.dropna()
 	# plot chart
-	fig = plt.figure(4)
+	fig = plt.figure()
 	volumebP.plot.barh(title = 'Purcharser by volume')
 	plt.yticks( fontsize=5)
 	plt.savefig('volume.png', bbox_inches='tight',dpi=100)
@@ -138,12 +142,23 @@ def detectAnomalies(df):
 	output = pd.concat(frames)
 	output.to_csv('errorReport.csv')
 
+#Tabular data with mean and max price for a book wrt to its marking
+def groupByMarking(df):
+	
+	
+	df['total'] = df['Pounds'] + df['Pence']/240.0 + df['Shillings']/20.0
+	df['Copies Ordered'] = pd.to_numeric(df['Copies Ordered'], errors='coerce')
+	df['total'] = pd.to_numeric(df['total'], errors='coerce')
+	#df['average'] = df['total'].truediv(df['Copies Ordered'],fill_value = None)
+	describ = df.groupby(['Title', 'Code / Notes'])['average'].describe()
+	#describ.to_csv('groupByMarkingReport.csv')
 
 
 
 #plotTotalSale(df)
-computeIncome(df)
+#computeIncome(df)
 #purchaserVolume(df)
 #diffFilledPayment(df)
 #detectAnomalies(df)
+groupByMarking(df)
 
